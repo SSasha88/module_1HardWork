43a28e6cdc728d13da72538301ffc7a99140eb2b
SSasha88 <d1rus1853@gmail.com>
2024-11-14 10:16:46 +0300

    Упростил решение и добавил ввод имени

--- module_1HardWork.py
+++ module_1HardWork.py
@@ -1,6 +1,12 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-sorted(list(students))
-students1=sorted(list(students))
-final = students1[0],(sum((grades [0])))/len(grades[0]), students1[1],(sum((grades [1])))/len(grades[1]), students1[2],(sum((grades [2])))/len(grades[2]), students1[3],(sum((grades [3])))/len(grades[3]), students1[4],(sum((grades [4])))/len(grades[4])
-print(final)
+A = sorted(list(students))
+B = sum((grades [0]))/len(grades[0])
+C = sum((grades [1]))/len(grades[1])
+D = sum((grades [2]))/len(grades[2])
+E = sum((grades [3]))/len(grades[3])
+F = sum((grades [4]))/len(grades[4])
+C = {A[0]:B, A[1]:C, A[2]:D, A[3]:E, A[4]:F}
+print('Средний бал', C[input("Напишите имя студента: ")])    # усложнил готовое решение
+print('Cредний балл каждого ученика:', C)
+
